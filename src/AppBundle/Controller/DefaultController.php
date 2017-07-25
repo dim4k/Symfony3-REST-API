d8579480b58af17d33aea07d4fd492deca24af5b
antoine <kima0012@gmail.com>
2017-07-25 18:23:48 +0200

    add default webpage (content-type : html)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,25 @@
 
 namespace AppBundle\Controller;
 
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class DefaultController extends FOSRestController
 {
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+		$view = new View(null, 200);
+
+		$view->setFormat('html')
+			->setHeader('Content-Type', 'html')
+			->setTemplate("default/index.html.twig");
+
+		return $this->handleView($view);
     }
 }
